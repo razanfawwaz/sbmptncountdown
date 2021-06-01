088d22e6978e27a2d03dd201b88cd08377af444e
Razan Fawwaz <40789105+razanfawwaz@users.noreply.github.com>
2021-06-01 18:58:05 +0700

    Update deadline.js

--- deadline.js
+++ deadline.js
@@ -1,3 +1,3 @@
 var sbmptn_current_time, sbmptn_deadline_time;
 sbmptn_current_time = new Date();
-sbmptn_deadline_time = '14 Aug 2020 15:00:00 +0700';
+sbmptn_deadline_time = '14 Jun 2021 15:00:00 +0700';
